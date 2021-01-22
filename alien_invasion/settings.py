1fc98f2165cb6232f99591ed5f7c872a23368e8d
Carolyn Whelpley <carolynwhelpley@carolyns-air.lan>
2021-01-22 09:39:36 -0500

    updated ship and alien invasion

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,6 +1,11 @@
 class Settings:
-
+  screen_width = 1200
+  screen_height = 800
+  bg_color = 'green'
+  ship_speed = 1.5
+  
   def _init_(self):
     self.screen_width = 1200
     self.screen_height = 800
     self.bg_color = (230, 230, 230)
+    self.ship_speed = 1.5
